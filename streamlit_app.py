11ab4c131f857a7f8347557d3b1bf2f59e49c110
Elana Savita Alphonso <elalphon@syr.edu>
2025-12-01 03:35:28 +0000

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="🧬 Medical RAG Chatbot", page_icon="🧬", layout="centered")
 
-old = st.Page("standalone.py", icon="📘", title="News Letters")
-
 new = st.Page("app.py", title="Medical RAG Chatbot", icon="🧬", default=True)
 
-nav = st.navigation({"Labs": [old,new]})
+nav = st.navigation({"Labs": [new]})
 nav.run()
